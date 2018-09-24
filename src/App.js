3482f934e5d3d0fb2824106d48c0b0f29af860c0
Charlie Wang <charliewang@Charlies-MacBook-Pro-2.local>
2018-09-24 17:20:48 -0400

    master commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RoomList from './components/RoomList'
+import * as firebase from 'firebase';
+
+<script src="https://www.gstatic.com/firebasejs/5.5.1/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDGwZlDzqQZ3RGAzrqBR8VV2UcOYFYNvSQ",
+    authDomain: "chatroom-e3a23.firebaseapp.com",
+    databaseURL: "https://chatroom-e3a23.firebaseio.com",
+    projectId: "chatroom-e3a23",
+    storageBucket: "chatroom-e3a23.appspot.com",
+    messagingSenderId: "3749167464"
+  };
+  firebase.initializeApp(config);
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="chatroom">
+      <RoomList
+      firebase={this.props.firebase()}
+      />
+
+
+    );
+  }
+}
+
+export default App;
