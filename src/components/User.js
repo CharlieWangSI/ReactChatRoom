eb1401403860f54326bf21d9834fe6ad2aa36e68
Charlie Wang <charliewang@Charlies-MBP-2.fios-router.home>
2018-10-02 22:12:13 -0400

    checkpoint-5-set-username first draft

---
+++ src/components/User.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+      this.props.firebase.auth().onAuthStateChanged( user => {
+  this.props.setUser(user);
+})
+  }
+
+  render(){
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    return(
+      <div>
+      <input type="button" value="signIn" onChange={this.props.firebase.auth().signInWithPopup(provider)}/>
+      <input type="button" value="signOut" onChange={this.props.firebase.auth().signOut()}/>
+      <div>{this.props.user.displayName}</div>
+      </div>
+    )
+  }
+}
+
+
+export default User;
